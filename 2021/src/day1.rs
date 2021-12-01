88b56438edc3de72736e1120a584e399dfbeb4aa
Yann Jorelle <yannjorelle@protonmail.com>
2021-12-01 20:07:16 +0200

    Day 1 of 2021

---
+++ 2021/src/day1.rs
@@ -0,0 +1,29 @@
+use std::fs::read_to_string;
+
+fn part_one(measurements: &[u32]) -> u32 {
+    let mut count = 0;
+    for i in 1..measurements.len() {
+        if measurements[i] > measurements[i - 1] {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn part_two(measurements: &[u32]) -> u32 {
+    let mut sums: Vec<u32> = Vec::new();
+    for i in 2..measurements.len() {
+        sums.push(measurements[i - 2] + measurements[i - 1] + measurements[i]);
+    }
+    part_one(&sums)
+}
+
+fn main() {
+    let input_string = read_to_string("./inputs/day1.txt").expect("Input file not found");
+    let measurements = input_string
+        .lines()
+        .map(|i| i.parse::<u32>().expect("Failed to parse measurement value"))
+        .collect::<Vec<_>>();
+    println!("Part 1 solution: {}", part_one(&measurements));
+    println!("Part 2 solution: {}", part_two(&measurements));
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Advent of Code 2020
+# Advent of Code
 
-My solutions to [Advent of Code 2020](https://adventofcode.com/2020).
+My solutions to [Advent of Code](https://adventofcode.com).
